b59ea70f7d513159a68df5e6df6d4b8f39d2874a
Daan Davidsz <wtsdaan@gmail.com>
2011-01-06 09:08:36 +0100

    Added better cherry

--- bonus/Cherry.cpp
+++ bonus/Cherry.cpp
@@ -1,6 +1,9 @@
 #include "Cherry.h"
 
 void Cherry::render(float ticks, float gameTime) {
+    float height = 0.2 * sin(M_PI * (gameTime * 1.5));
+    glTranslatef(0, 0, height);
+
     glRotatef((int)(gameTime * 180) % 360, 0, 0, 1);    
     
     glColor3f(0.65, 0, 0);
@@ -15,9 +18,9 @@ void Cherry::render(float ticks, float gameTime) {
     quadobj = gluNewQuadric();
     
     glRotatef(-45, 0, 0, 1);    
-    glRotatef(24, 1, 0, 0);    
+    glRotatef(35, 1, 0, 0);    
 
-    gluCylinder(quadobj, 0.07, 0.07, 1.1, 8, 8);
+    gluCylinder(quadobj, 0.07, 0.07, 1.3, 8, 8);
     
     glRotatef(-24, 1, 0, 0);     
     glRotatef(45, 0, 0, 1);       
